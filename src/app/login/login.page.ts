29cc2acdec414b55b4c07b221a0dea1bd4018ff4
pcortesduoc <p.cortes@duocuc.cl>
2022-09-05 03:38:06 -0400

    Tomar datos y enviarlos con state navigationextras

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  //Inicializar los datos a guardar de la pagina, con un valor inicial vacio
+  user={
+    email:"",
+    password:""
+  }
+
+  constructor(private router: Router) { }//debe instanciar router para poder usarlo
 
   ngOnInit() {
+  }
+
+  //funcion para guardar datos del formulario en state, navegar a otra pagina y llevar esos datos con navigation extras
+  login(){
+    let NavigationExtras: NavigationExtras = {
+      state: {
+        user: this.user
+      }
+    };
+    this.router.navigate(['/home'],NavigationExtras)
   }
 
 }
