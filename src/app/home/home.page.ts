20d190fc224ba467b177fd753153225a7356dc3d
Patricio Villarroel <85747928+pat-ern@users.noreply.github.com>
2022-09-11 23:00:30 -0300

    mejoras home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -28,6 +28,37 @@ export class HomePage {
   });
   }
 
+<<<<<<< Updated upstream
+=======
+  ngAfterViewInit() {
+    const titulo = this.animationCtrl.create()
+      .addElement(this.titulo.nativeElement)
+      .keyframes([
+        { offset: 0, transform: 'scale(1) rotate(0)' },
+        { offset: 0.5, transform: 'scale(1.1) rotate(0)', opacity: '0.1'  },
+        { offset: 1, transform: 'scale(1) rotate(0)' }
+      ]);
+
+    const qr = this.animationCtrl.create()
+      .addElement(this.qr.nativeElement)
+      .keyframes([
+        { offset: 0, transform: 'scale(1.2) rotate(0)' },
+        { offset: 0.5, transform: 'scale(1.1) rotate(0)'},
+        { offset: 1, transform: 'scale(1) rotate(0)' }
+      ]);
+
+    const animacion = this.animationCtrl
+      .create()
+      .duration(2000)
+      .iterations(1)
+      .addAnimation([titulo, qr]);
+      //.addAnimation([hola1, squareA, squareB, squareC, otra]);
+
+    animacion.play();
+  }
+  
+
+>>>>>>> Stashed changes
   openFirst() {
     this.menu.enable(true, 'first');
     this.menu.open('first');
