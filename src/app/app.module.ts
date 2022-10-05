6cfe5e47c5e4e33afe38c881cda600d95c59080b
asdf <asdf>
2022-10-05 17:12:04 -0300

    Api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { HttpClientModule } from '@angular/common/http';
+
 // Base de datos sqlite y localstorage
 import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
 import { IonicStorageModule } from '@ionic/storage-angular';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule, ReactiveFormsModule, IonicStorageModule.forRoot()],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule, ReactiveFormsModule, IonicStorageModule.forRoot(),HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, SQLite],
   bootstrap: [AppComponent],
 })
--- src/app/pages/historial/historial.page.ts
+++ src/app/pages/historial/historial.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
+
 
 @Component({
   selector: 'app-historial',
@@ -8,11 +10,86 @@ import { Router } from '@angular/router';
 })
 export class HistorialPage implements OnInit {
 
-  constructor(private router: Router) { 
+  id:any;
+  codigo:any;
+  nombre: any;
+  seccion: any;
+  profesor: any;
+  sede: any;
+
+  constructor(private router: Router, private api: ApiService) { 
     this.router.navigate(['historial/menu'])
   }
 
-  ngOnInit() {
+  ionViewWillEnter(){
+    this.getUsuarios();
+    this.getPosts();
   }
-
+  getUsuario(userId){
+    this.api.getUsuario(userId).subscribe((data)=>{
+      console.log(data)
+      this.user=data;
+    });
+  }
+  getUsuarios(){
+    this.api.getUsuarios().subscribe((data)=>{
+      this.users=data;
+    });
+  }
+  getPosts(){
+    this.api.getPosts().subscribe((data)=>{
+      this.posts=data;
+      this.posts.reverse();
+    });
+  }
+  guardarPost(){
+    if(this.post.userId==null){
+      if(this.user==undefined){
+        console.log("Seleccione un usuario");
+        return;
+      }
+      this.post.userId=this.user.id;
+      this.api.createPost(this.post).subscribe(
+        ()=>{
+          console.log("Creado Correctamente");
+          this.getPosts();
+        },
+        error=>{
+          console.log("Error "+error)
+        }
+      );
+    }
+    else{
+      this.api.updatePost(this.post.id,this.post).subscribe(
+        ()=>{
+          console.log("Actualzado Correctamente");
+          this.getPosts();
+        },
+        error=>{
+          console.log("Error "+error)
+        }
+      );
+    }
+  }
+  setPost(_post){
+    this.post=_post;
+    this.getUsuario(_post.userId);
+    this.compareWith = this.compareWithFn;
+  }
+  eleminarPost(_post){
+    console.log("eeliminar")
+    this.api.deletePost(_post.id).subscribe(
+      success=>{
+        console.log("Eliminado correctamente");
+        this.getPosts();
+      },
+      error=>{
+        console.log("Error "+error)
+      }
+    )
+  }
+  compareWithFn = (o1, o2) => {
+    return o1 && o2 ? o1.id === o2.id : o1 === o2;
+  };
 }
+ 
