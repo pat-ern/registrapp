2b291faf116f7cb676c7ad03b2f10cfaf75e12af
Patricio Villarroel <pat.villarroel@duocuc.cl>
2022-10-12 18:18:38 -0300

    cambios en pag historial
    simulación de registro semanal mediante algoritmo, consumiendo la misma api

--- src/app/pages/historial/historial.page.ts
+++ src/app/pages/historial/historial.page.ts
@@ -11,6 +11,10 @@ import { ApiService } from 'src/app/services/api.service';
 export class HistorialPage implements OnInit {
 
   listaAsignaturas: any;
+  listaOrdenada: any;
+
+
+  asignaturas: Array<any> = [];
 
   //id:any;
   //codigo:any;
@@ -28,11 +32,67 @@ export class HistorialPage implements OnInit {
     //this.getPosts();
   }
 
+  diaSemana(dia: number){
+    switch(dia){
+      case 1:
+        return "lunes";
+      case 2:
+        return "martes";
+      case 3:
+        return "miercoles";
+      case 4:
+        return "jueves";
+      case 5:
+        return "viernes";
+      case 6:
+        return "sabado";
+      case 7:
+        return "domingo";
+    }
+  }
+
   crearListaAsignaturas(){
     this.api.getAsignaturas().subscribe((data)=>{
-      this.listaAsignaturas = data.asignatura; // data es el json que devuelve el api, asignatura es el nombre del array
-      console.log(this.listaAsignaturas);
+
+      this.listaAsignaturas = data.asignatura // data es el json que devuelve el api, listaAsignaturas es el array
+
+      for (let i = 0; i < this.listaAsignaturas.length; i++) {
+        for (let j = 0; j < this.listaAsignaturas[i].horario.length; j++) {
+
+          // se guardan datos en variable de asignatura 
+          let asignatura = {
+            codigo: " ",
+            dia: 0,
+            hora: " "
+          }
+
+          asignatura.codigo = this.listaAsignaturas[i].codigo;
+          asignatura.dia = this.listaAsignaturas[i].horario[j].dia;
+          asignatura.hora = this.listaAsignaturas[i].horario[j].hra_ini;
+
+          this.asignaturas.push(asignatura);
+
+        }
+      
+      }
+
+      // ordenar lista de asignaturas por dia
+      //this.listaOrdenada = this.asignaturas.sort((a, b) => a.dia - b.dia);
+
+      // ordenar lista de asignaturas por dia y hora 
+      this.listaOrdenada = this.asignaturas.sort((a, b) => a.dia - b.dia || a.hora.localeCompare(b.hora));
+
+      // cambiar dia de numero a nombre
+      for (let i = 0; i < this.listaOrdenada.length; i++) {
+        this.listaOrdenada[i].dia = this.diaSemana(this.listaOrdenada[i].dia);
+      }
+      
+      console.log(this.listaOrdenada);
+
     });
+
+
+
   }
 
   // getUsuario(userId){
