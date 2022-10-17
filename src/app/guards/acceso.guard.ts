f85f0bcf940cfc9334ab44df711028986e79f87a
asdf <asdf>
2022-10-17 15:05:55 -0300

    Modificar guard

--- src/app/guards/acceso.guard.ts
+++ src/app/guards/acceso.guard.ts
@@ -16,12 +16,26 @@ export class AccesoGuard implements CanActivate {
 
     const hora = new Date().getHours();
 
+    /* //este es el ejemplo de guard
     let horaInicio = 18;
     let horaFin = 24;
 
     if (hora >= horaInicio && hora <= horaFin) {
       this.router.navigate(['home']);
       console.log('AccesoGuard: No puede acceder entre las '+horaInicio+' y las '+horaFin+' horas');
+      return false;
+    } 
+    */
+
+
+    //podemos hacer que el escaneo qr solo sea accesible por 30 mins o 1 hora desde que empieza la clase
+    let horaInicioClase1 = 8;
+    let horaFinClase1 = 9; //podria poner 8:30
+    // agregar horas para cada clase del dia, quizas hay otra forma msa eficiente
+
+    if (hora < horaInicioClase1 || hora > horaFinClase1) { //agregar todas las horas
+      this.router.navigate(['home']);
+      //console.log('AccesoGuard: No puede acceder entre las '+horaInicio+' y las '+horaFin+' horas');
       return false;
     } 
 
