812804951912ad0d637be7a8fba000afa25e0982
Patricio Villarroel <pat.villarroel@duocuc.cl>
2022-11-09 18:19:17 -0300

    limpieza codigo servicios

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -15,10 +15,9 @@ export class ApiService {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*'
       })
-  } // Se establece la base url del API a consumir
+  } 
   
- apiURL = 'https://pcortesduoc.github.io/movilApi/clase.json' // Fuente Original funciona solo get
- //apiURL = 'http://192.168.0.6:3000'; // Ejecuta json-server -H ip .\db.json para ejecutar un Fake APIRest
+ apiURL = 'https://pcortesduoc.github.io/movilApi/clase.json'
 
   constructor(private http: HttpClient) { }
 
@@ -26,28 +25,6 @@ export class ApiService {
     return this.http.get(this.apiURL).pipe(
       retry(3)
     );
-  }
-
-  // otros metodos
-
-  getPosts(): Observable<any> {
-    return this.http.get(this.apiURL + '/posts/').pipe(retry(3));
-  }
-
-  getPost(id): Observable<any> {
-    return this.http.get(this.apiURL + '/posts/' + id).pipe(retry(3));
-  }
-
-  createPost(post): Observable<any> {
-    return this.http.post(this.apiURL + '/posts', post, this.httpOptions).pipe(retry(3));
-  }
-
-  updatePost(id, post): Observable<any> {
-    return this.http.put(this.apiURL + '/posts/' + id, post, this.httpOptions).pipe(retry(3));
-  }
-
-  deletePost(id): Observable<any> {
-    return this.http.delete(this.apiURL + '/posts/' + id, this.httpOptions);
   }
 
 }
