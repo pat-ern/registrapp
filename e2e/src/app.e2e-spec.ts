9a38a9b74a36e2135b02435ef25c59d86e8fc88c
Patricio Villarroel <pat.villarroel@duocuc.cl>
2022-11-18 22:58:58 -0300

    testing empieza a funcionar en login

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,35 @@
-import { AppPage } from './app.po';
+import { ExpectedConditions } from 'protractor';
+import { AppPage } from './app.po';;
 
 describe('new App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
+    page.navigateTo();
   });
 
-  it('should be blank', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toContain('Start with Ionic UI Components');
+  it('should get email input label', async () => {
+    
+    expect(page.getLabelText()).toContain('Hola');
+    // check if login button is enabled after input is typed
+    //expect(page.getLoginButton().isEnabled()).toBeTruthy();
   });
+
+  //it('should type password', async () => {
+    //expect(page.typeLoginInputPassword());
+    // check if login button is enabled after input is typed
+    //expect(page.getLoginButton().isEnabled()).toBeTruthy();
+  //});
+
+  //it('should enable login', async () => {
+    //expect(page.getLoginButton().isEnabled()).toBeTruthy();
+  //});
+
+  // obtener el texto del boton login 
+  //it('should get login button text', async () => {
+    //var texto_boton = page.getLoginButton().getText();
+    //console.log(texto_boton);
+  //});
+
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,16 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ExpectedConditions } from 'protractor';
 
 export class AppPage {
+
+  validUsername = 'testmail@domain.com';
+  validPassword = '123456';
+
   navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.deepCss('app-root ion-content')).getText();
+  getLabelText() {
+    return element(by.css('ion-label[name=userinputlabel]')).getText();
   }
+
 }
