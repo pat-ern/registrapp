21e9baf55aff6bd70f3d61daddefd98b7f56cec1
pcortesduoc <p.cortes@duocuc.cl>
2022-11-26 03:45:38 -0300

    Actualizar api key

--- src/app/services/api-correos.service.ts
+++ src/app/services/api-correos.service.ts
@@ -17,9 +17,7 @@ export class ApiCorreosService {
   */
 
   apiURL = 'https://api.mailgun.net/v3/sandboxf43489a9f7614e84af8f183724d6c85f.mailgun.org/messages'  //hay que agregarle /messages al final del link
-  apiKey = 'key-a8af3df8a00d2778589ab927646bf223'
-  //oldApiKey = '9fe0a678d85579ad66c05a7c69689aa5-2de3d545-23ea1fbc' //esta era la api key antes de que me la bloquearan
-  //smtpPass = '7e89d4e1b3eb3d25bc2e5f7f7e47a3ca-2de3d545-ff4afc1d'  // este se usa en un metodo alternativo de enviar correos, no esta implementado aqui
+  apiKey = 'key-c12bf806897e339e387d0515fd70c89e'
 
   constructor(private http: HttpClient) { }
 
